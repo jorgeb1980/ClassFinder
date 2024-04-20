3b8a8d580c7d55bfe628f8e02ca800f4a5695996
Jorge Bau Gracia <jbaugracia@gmail.com>
2024-04-20 21:56:32 +0200

    Some more changes to make the code prettier and more compact

--- src/test/java/test/files/TestJars.java
+++ src/test/java/test/files/TestJars.java
@@ -1,5 +1,6 @@
 package test.files;
 
+import static java.nio.charset.Charset.forName;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static test.files.FileUtils.addToContainer;
@@ -51,22 +52,22 @@ public class TestJars extends BaseFileTest {
 		try {
 			var container = createContainer(tempDir, "resource.jar");
 			addToContainer(
-					container, 
-					"resource.txt", 
-					"",
-					"contents of the file".getBytes(Charset.forName("utf-8"))
+				container, 
+				"resource.txt", 
+				"",
+				"contents of the file".getBytes(forName("utf-8"))
 			);
 			addToContainer(
-					container, 
-					"resource2.txt", 
-					"",
-					"contents of the second file".getBytes(Charset.forName("utf-8"))
+				container, 
+				"resource2.txt", 
+				"",
+				"contents of the second file".getBytes(forName("utf-8"))
 			);
 			addToContainer(
-					container, 
-					"resource3.txt", 
-					"a/b/c/",
-					"contents of the third file".getBytes(Charset.forName("utf-8"))
+				container, 
+				"resource3.txt", 
+				"a/b/c/",
+				"contents of the third file".getBytes(forName("utf-8"))
 			);
 			// 3 files named liked this inside the container
 			var result1 = ResourceSearcher.SEARCHER.search(tempDir, "resource");
@@ -89,37 +90,34 @@ public class TestJars extends BaseFileTest {
 		try {
 			// 1 resource in container 1
 			var container1 = createContainer(tempDir, "resource1.jar");
+			final Charset charset = forName("utf-8");
 			addToContainer(
-					container1, 
-					"resource1_1.txt", 
-					"",
-					"contents of the file, first container".
-						getBytes(Charset.forName("utf-8"))
+				container1, 
+				"resource1_1.txt", 
+				"",
+				"contents of the file, first container".getBytes(charset)
 			);
 			// 2 resources in container 2
 			var container2 = createContainer(tempDir, "resource2.jar");
 			addToContainer(
-					container2, 
-					"resource2_1.txt", 
-					"",
-					"contents of the first file, second container".
-						getBytes(Charset.forName("utf-8"))
+				container2, 
+				"resource2_1.txt", 
+				"",
+				"contents of the first file, second container".getBytes(charset)
 			);
 			addToContainer(
-					container2, 
-					"resource2_2.txt", 
-					"",
-					"contents of the second file, second container".
-						getBytes(Charset.forName("utf-8"))
+				container2, 
+				"resource2_2.txt", 
+				"",
+				"contents of the second file, second container".getBytes(charset)
 			);
 			// 1 resource in container 3
 			var container3 = createContainer(tempDir, "resource3.jar");
 			addToContainer(
-					container3, 
-					"resource3_1.txt", 
-					"a/b/c/",
-					"contents of the first file, third container".
-						getBytes(Charset.forName("utf-8"))
+				container3, 
+				"resource3_1.txt", 
+				"a/b/c/",
+				"contents of the first file, third container".getBytes(charset)
 			);
 			// Simple search
 			var result1 = ResourceSearcher.SEARCHER.search(tempDir, "resource");
@@ -143,63 +141,58 @@ public class TestJars extends BaseFileTest {
 			// dir 1: 1 container with 2 resources
 			var subDir1 = FileUtils.createSubDir(tempDir, "level1\\level1_1");
 			var container11 = createContainer(subDir1, "resource1_1.jar");
+			final Charset charset = forName("utf-8");
 			addToContainer(
-					container11, 
-					"resource11_1.txt", 
-					"first/",
-					"contents of the first file, first container, first directory".
-						getBytes(Charset.forName("utf-8"))
+				container11, 
+				"resource11_1.txt", 
+				"first/",
+				"contents of the first file, first container, first directory".getBytes(charset)
 			);
 			addToContainer(
-					container11, 
-					"resource11_2.txt", 
-					"a/",
-					"contents of the first file, first container, first directory".
-						getBytes(Charset.forName("utf-8"))
+				container11, 
+				"resource11_2.txt", 
+				"a/",
+				"contents of the first file, first container, first directory".getBytes(charset)
 			);			
 			// dir 2: 2 containers with 1 resource each one
 			var subDir2 = FileUtils.createSubDir(tempDir, "level1/level1_2");
 			var container21 = createContainer(subDir2, "resource2_1.jar");
 			addToContainer(
-					container21, 
-					"resource21_1.txt", 
-					"",
-					"contents of the first file, first container, second directory".
-						getBytes(Charset.forName("utf-8"))
+				container21, 
+				"resource21_1.txt", 
+				"",
+				"contents of the first file, first container, second directory".getBytes(charset)
 			);
 			var container22 = createContainer(subDir2, "resource2_2.jar");
 			addToContainer(
-					container22,
-					"resource22_1.txt", 
-					"",
-					"contents of the first file, second container, second directory".
-						getBytes(Charset.forName("utf-8"))
+				container22,
+				"resource22_1.txt", 
+				"",
+				"contents of the first file, second container, second directory".getBytes(charset)
 			);
 			// dir 3: 5 containers, 1 with 2 resources, 4 empty
 			var subDir3 = FileUtils.createSubDir(tempDir, "level1/level1_3");
 			var container31 = createContainer(subDir3, "resource3_1.jar");
 			addToContainer(
-					container31, 
-					"resource31_1.txt", 
-					"a/b/c/",
-					"contents of the first file, first container, third directory".
-						getBytes(Charset.forName("utf-8"))
+				container31, 
+				"resource31_1.txt", 
+				"a/b/c/",
+				"contents of the first file, first container, third directory".getBytes(charset)
 			);
 			addToContainer(
-					container31, 
-					"resource31_2.txt", 
-					"a/b/c/",
-					"contents of the second file, first container, third directory".
-						getBytes(Charset.forName("utf-8"))
+				container31, 
+				"resource31_2.txt", 
+				"a/b/c/",
+				"contents of the second file, first container, third directory".getBytes(charset)
 			);
 			var container32 = createContainer(subDir3, "resource3_2.jar");
 			var container33 = createContainer(subDir3, "resource3_3.jar");
 			var container34 = createContainer(subDir3, "resource3_4.jar");
 			var container35 = createContainer(subDir3, "resource3_5.jar");
 			// Search certain files in the directories
-			File[] files = new File[]{subDir1, subDir2, subDir3};
-			SearchResult result1 = ResourceSearcher.SEARCHER.search(
-				Arrays.asList(files), "resource1");
+			var files = new File[]{subDir1, subDir2, subDir3};
+			
+			var result1 = ResourceSearcher.SEARCHER.search(Arrays.asList(files), "resource1");
 			printResults(files, result1);
 			assertEquals(2, result1.getResourcesByFile().get(container11).size());
             assertNull(result1.getResourcesByFile().get(container21));
@@ -209,8 +202,8 @@ public class TestJars extends BaseFileTest {
 			assertNull(result1.getResourcesByFile().get(container33));
 			assertNull(result1.getResourcesByFile().get(container34));
 			assertNull(result1.getResourcesByFile().get(container35));
-			SearchResult result2 = ResourceSearcher.SEARCHER.search(
-					Arrays.asList(files), "resource2");
+			
+			var result2 = ResourceSearcher.SEARCHER.search(Arrays.asList(files), "resource2");
 			printResults(files, result2);
 			assertNull(result2.getResourcesByFile().get(container11));
 			assertEquals(1, result2.getResourcesByFile().get(container21).size());
@@ -220,8 +213,8 @@ public class TestJars extends BaseFileTest {
 			assertNull(result2.getResourcesByFile().get(container33));
 			assertNull(result2.getResourcesByFile().get(container34));
 			assertNull(result2.getResourcesByFile().get(container35));
-			SearchResult result3 = ResourceSearcher.SEARCHER.search(
-					Arrays.asList(files), "resource3");
+			
+			var result3 = ResourceSearcher.SEARCHER.search(Arrays.asList(files), "resource3");
 			printResults(files, result3);
 			assertNull(result3.getResourcesByFile().get(container11));
 			assertNull(result3.getResourcesByFile().get(container21));
